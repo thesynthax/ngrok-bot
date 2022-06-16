dbcba9bd945d1e350fceb89a4e057c149d590035
Kushagra Srivastava <kushagra1403@gmail.com>
2022-06-16 14:07:13 +0530

    bot

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 import time
+import discord_webhook
 
 driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
+URL = "localhost:4040"
+
 urls = []
 
-driver.get("localhost:4040")
+driver.get(URL)
 urlList = driver.find_elements_by_tag_name("a")
 for url in urlList:
     if "tcp" in url.text:
         urls.append(url.text)
 
-for i in urls:
-    print(i)
-
 time.sleep(10)
+
+discord_webhook.send_msg(ssh=urls[0], vnc=urls[1])
+
 driver.quit()
