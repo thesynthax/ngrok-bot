ba1c0a45e357dc21d00c6d6087ea424e2713a1f0
Kushagra Srivastava <kushagra1403@gmail.com>
2022-07-01 14:21:32 +0530

    misc

--- discord_webhook.py
+++ discord_webhook.py
@@ -1,17 +1,19 @@
 from discord_webhooks import DiscordWebhooks
 
-webhook_url = ''
+webhook_url = 'https://discord.com/api/webhooks/986912518159106068/eklOK5k5lAZjveUS-p7Z_ciongoZd-3cDlIEhk4Vk5BJ4GGSfdwi-CRsmojewAo5sysv'
 
 def send_msg(ssh, vnc):
 
     WEBHOOK_URL = webhook_url 
 
+    message = ssh + "\n" + vnc
+
     webhook = DiscordWebhooks(WEBHOOK_URL)
     webhook.set_footer(text='-- thesynthax')
 
     webhook.set_content(title='Raspberry Pi Online', description="Here are the port numbers.")
-    webhook.add_field(name='SSH/VNC: ', value="")
-    webhook.add_field(name='-> ', value=ssh + "\n" + vnc)
+
+    webhook.add_field(name='SSH/VNC: ', value=message)
 
     webhook.send()
 
